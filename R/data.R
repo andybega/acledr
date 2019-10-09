cda5fde70b31ae01472e2f63d9aac7c69b777ec7
Andreas Beger <adbeger@gmail.com>
2019-10-09 10:56:29 +0300

    add coverage data

---
+++ R/data.R
@@ -0,0 +1,6 @@
+#' ACLED country coverage
+#'
+#' A dataset containing the start of ACLED data coverage for a country
+#'
+#' @source \url{https://www.acleddata.com/wp-content/uploads/dlm_uploads/2018/02/Country-and-Time-Period-coverage_updatedJune2019-1.pdf}
+"acled_coverage"
---
+++ data-raw/acled_coverage.R
@@ -0,0 +1,44 @@
+#
+#   Extract country coverage from relevant PDF note
+#
+
+library("httr")
+library("tabulizer")
+library("stringr")
+library("countrycode")
+
+url <- "https://www.acleddata.com/wp-content/uploads/dlm_uploads/2018/02/Country-and-Time-Period-coverage_updatedJune2019-1.pdf"
+
+r <- httr::GET(url)
+if (httr::status_code(r)!=200) {
+  stop("URL seems to not be valid anymore, response code ", httr::status_code(r))
+}
+
+writeBin(content(r, as = "raw"), "data-raw/pdf/coverage.pdf")
+
+coverage <- extract_tables("data-raw/pdf/coverage.pdf")
+coverage <- do.call(rbind, coverage)
+coverage <- as.data.frame(coverage)
+colnames(coverage) <- c("country", "start")
+coverage <- coverage[!coverage$start %in% c("", "Time period"), ]
+
+date <- str_extract(coverage$start, "[0-9/]+")
+date <- paste0("1/", date)
+coverage$start <- as.Date(date, format = "%d/%m/%Y")
+
+coverage$gwcode <- countrycode(coverage$country, "country.name", "cown")
+coverage$gwcode[coverage$country=="Serbia"]   <- 340
+coverage$gwcode[coverage$country=="eSwatini"] <- 572
+coverage$gwcode[coverage$country=="Yemen"]    <- 678
+coverage$gwcode[coverage$country=="Vietnam"]  <- 816
+
+coverage <- coverage[, c("country", "gwcode", "start")]
+coverage <- coverage[order(coverage$country), ]
+
+acled_coverage <- coverage
+
+usethis::use_data(acled_coverage, overwrite = TRUE)
+
+
+
+
